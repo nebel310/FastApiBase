cfe41c69ec15c2da8cba7c962e6cb05d5f4d2a9b
Vlados <vladgrigorea@gmail.com>
2025-02-23 21:40:04 +0300

    Dada

---
+++ main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from database import create_tables, delete_tables
+from router.auth import router as auth_router
+
+
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print('База очищена')
+    await create_tables()
+    print('База готова к работе')
+    yield
+    print('Выключение')
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Your App",
+        version="1.0.0",
+        description="API for users",
+        routes=app.routes,
+    )
+    openapi_schema["components"]["securitySchemes"] = {
+        "Bearer": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT"
+        }
+    }
+    if "/auth/me" in openapi_schema["paths"]:
+        openapi_schema["paths"]["/auth/me"]["get"]["security"] = [{"Bearer": []}]
+        openapi_schema["paths"]["/auth/logout"]["post"]["security"] = [{"Bearer": []}]
+    
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app = FastAPI(lifespan=lifespan)
+app.openapi = custom_openapi
+app.include_router(auth_router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],  # Тут адрес фронтенда
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
